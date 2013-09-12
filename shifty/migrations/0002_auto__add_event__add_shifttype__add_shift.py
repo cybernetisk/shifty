86b1315752dc8cf1880ee82db271486f28a13431
Henrik Steen <henrist@henrist.net>
2013-09-12 19:38:22 +0200

    Added models for event, shift and shifttype.

---
+++ shifty/migrations/0002_auto__add_event__add_shifttype__add_shift.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Event'
+        db.create_table(u'shifty_event', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('start', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal(u'shifty', ['Event'])
+
+        # Adding model 'ShiftType'
+        db.create_table(u'shifty_shifttype', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'shifty', ['ShiftType'])
+
+        # Adding model 'Shift'
+        db.create_table(u'shifty_shift', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('event', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['shifty.Event'])),
+            ('shift_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['shifty.ShiftType'])),
+            ('volunteer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('start', self.gf('django.db.models.fields.DateTimeField')()),
+            ('stop', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal(u'shifty', ['Shift'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Event'
+        db.delete_table(u'shifty_event')
+
+        # Deleting model 'ShiftType'
+        db.delete_table(u'shifty_shifttype')
+
+        # Deleting model 'Shift'
+        db.delete_table(u'shifty_shift')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'shifty.event': {
+            'Meta': {'object_name': 'Event'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'shifty.shift': {
+            'Meta': {'object_name': 'Shift'},
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shifty.Event']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shift_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shifty.ShiftType']"}),
+            'start': ('django.db.models.fields.DateTimeField', [], {}),
+            'stop': ('django.db.models.fields.DateTimeField', [], {}),
+            'volunteer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        u'shifty.shifttype': {
+            'Meta': {'object_name': 'ShiftType'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['shifty']
--- shifty/models.py
+++ shifty/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    start = models.DateTimeField()
+
+class Shift(models.Model):
+    event = models.ForeignKey("Event", null=False)
+    shift_type = models.ForeignKey("ShiftType", null=False)
+    volunteer = models.ForeignKey(User, null=True, blank=True)
+    start = models.DateTimeField()
+    stop = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.shift_type.title
+
+class ShiftType(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.title
