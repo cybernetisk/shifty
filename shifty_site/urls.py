f6d88c3f21474cd656a8dc963b269111416ad337
Øyvind Bakkeli <olivaq@gmail.com>
2013-09-12 20:31:12 +0200

    static files

--- shifty_site/urls.py
+++ shifty_site/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', 'shifty.views.index')
+    url(r'^$', 'shifty.views.index')
 )
