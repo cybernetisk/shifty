49c4c7a7641735dd876949e178f3253eb674e528
Sigurd Ljødal <sigurd@ljodal.no>
2013-09-12 19:58:21 +0200

    Add scss and handlebars assets

---
+++ watcher.sh
@@ -0,0 +1,12 @@
+
+# Run handlebars watcher
+#
+# To install:
+# npm install -g handlebar-rider
+handlebar-rider -r -w -i assets/handlebars -o shifty/static/js/templates.js &
+
+# Run sass watcher
+#
+# To install:
+# gem install sass --pre
+sass --sourcemap --watch assets/scss:shifty/static/css
