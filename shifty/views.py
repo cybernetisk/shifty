3afa106ea60f7c1b4ffa38bf86c7bfd1efff7acf
Mari Larsen <marilarsen87@gmail.com>
2013-09-19 21:42:10 +0200

    small changes

--- shifty/views.py
+++ shifty/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from shifty.models import Shift, Event, ShiftType
 
 def index(request):
     return render_to_response('shifty/index.html', {'name':'lol'})   
 
 # added by marill 
 def shifts(request):
-    return render_to_response('shifty/shifts.html')    
+    return render_to_response('shifty/shifts.html', {'shifts':Shift.objects.all()})    
