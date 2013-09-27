d553608ee88d9e40f1a476c1231b1a0b822ca3c2
Øyvind Bakkeli <olivaq@gmail.com>
2013-09-27 19:11:11 +0200

    Added more shift view sets

--- shifty/rest.py
+++ shifty/rest.py
@@ -1,13 +1,28 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from shifty.serializers import EventSerializer, ShiftSerializer
-from models import Event
+from shifty.serializers import EventSerializer, ShiftSerializer, ShiftTypeSerializer
+from models import Event, Shift, ShiftType
 
 
 class EventViewSet(viewsets.ModelViewSet):
 
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+
+class ShiftViewSet(viewsets.ModelViewSet):
+
+    queryset = Shift.objects.all()
+    serializer_class = ShiftSerializer
+
+
+
+class ShiftTypeViewSet(viewsets.ModelViewSet):
+
+    queryset = ShiftType.objects.all()
+    serializer_class = ShiftTypeSerializer
+
 
 
 """class ViewSet(viewsets.ModelViewSet):
