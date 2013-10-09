bef6b99cade79c01785db04b73b035ed989302c3
Henrik Steen <henrist@henrist.net>
2013-10-09 14:39:28 +0200

    Trenger pyyaml for fixtures.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='shifty',
       version="1",
       packages = ['shifty'],
-      install_requires = ['django','south', 'djangorestframework', 'markdown', 'django-filter']
+      install_requires = ['django','south', 'djangorestframework', 'markdown', 'django-filter', 'pyyaml']
       )
