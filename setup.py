9b98ae2d212c2fc806920f59c7c2d79a6b91e022
Øyvind Bakkeli <olivaq@gmail.com>
2014-01-24 19:48:01 +0100

    this looks prettier

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from distutils.core import setup
 setup(name='shifty',
       version="1",
       packages = ['shifty'],
-      install_requires = ['django','south', 'djangorestframework', 'markdown', 'django-filter', 'pyyaml']
+      install_requires = ['django',
+                          'south',
+                          'djangorestframework',
+                          'markdown',
+                          'django-filter',
+                          'pyyaml']
       )
