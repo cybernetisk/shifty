4fbb84a8d5ebc82dd47a3ffbbedce965e228572c
Henrik Steen <henrist@henrist.net>
2014-01-24 18:53:11 +0100

    Use ShiftSerializer in EventSerializer

--- shifty/serializers.py
+++ shifty/serializers.py
@@ -3,20 +3,20 @@ from models import Event, Shift, ShiftType
 from rest_framework import serializers
 
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('id', 'title', 'description', 'start', 'shifts')
-        depth = 1
-
-
 class ShiftSerializer(serializers.ModelSerializer):
     volunteer2 = serializers.Field(source='volunteer2')
     duration = serializers.Field(source='_duration')
     class Meta:
         model = Shift
-        fields = ('id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'volunteer2','duration')
+        fields = ('id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'volunteer2', 'duration')
 
+class EventSerializer(serializers.ModelSerializer):
+    shifts = ShiftSerializer(source='shifts')
+    
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'description', 'start', 'shifts')
+        depth = 1
 
 class ShiftTypeSerializer(serializers.ModelSerializer):
     class Meta:
