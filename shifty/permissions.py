cb0b1c295b17846617ba650f97cc7e244d2abc84
Øyvind Bakkeli <olivaq@gmail.com>
2014-03-18 17:57:28 +0100

    Fixed permission thingy

--- shifty/permissions.py
+++ shifty/permissions.py
@@ -1,11 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class isAdminOrReadOnly(BasePermission):
-	"""Checks if userlevel is Admin, if not, only allow SAFE_METHODS."""
-	def has_object_permission(self, request, view, obj):
-		print "lol"
-		return False
-		if request.method in SAFE_METHODS:
-			return True
-
-		return user.is_staff
+    """Checks if userlevel is Admin, if not, only allow SAFE_METHODS."""
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff
