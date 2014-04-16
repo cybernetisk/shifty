6f8cf71bd0a1866a03f5995c00337fcfd034dc37
Øyvind Bakkeli <olivaq@gmail.com>
2014-04-16 14:31:17 +0200

    Make sure we run migrations on other stuff to (reversions for now)

--- migrate.sh
+++ migrate.sh
@@ -3,5 +3,7 @@ if [ ! -f "shifty.sqlite" ]
 then
     python manage.py syncdb --noinput
 fi 
+
 python manage.py schemamigration shifty --auto
 python manage.py migrate shifty
+python manage.py migrate
