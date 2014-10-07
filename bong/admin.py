191fac7646911bf07fa083119d9a1bce0dbb2a36
steffennilsen <steffennilsen@gmail.com>
2014-10-07 17:50:47 +0200

    work on admin for bong

--- bong/admin.py
+++ bong/admin.py
@@ -3,9 +3,12 @@ from bong.models import BongWallet, BongLog
 
 class BongWalletAdmin(admin.ModelAdmin):
     list_display = ('user', 'balance')
+    search_fields = ['=user__username', ]
+    actions = None
 
 class BongLogAdmin(admin.ModelAdmin):
-    fields = ('user', 'action', 'date', 'modify')
+    list_display = ('user', 'action', 'date', 'modify')
+    actions = None
 
 admin.site.register(BongWallet, BongWalletAdmin)
 admin.site.register(BongLog, BongLogAdmin)
