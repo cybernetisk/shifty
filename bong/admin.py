5e8538d9598dacd1b4c0965af4934851bf8f5c8b
steffennilsen <steffennilsen@gmail.com>
2014-10-07 17:16:06 +0200

    working on admin integration of bong system

--- bong/admin.py
+++ bong/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from bong.models import BongWallet, BongLog
 
-from django import forms
-from django.db import models
-
 class BongWalletAdmin(admin.ModelAdmin):
     list_display = ('user', 'balance')
 
