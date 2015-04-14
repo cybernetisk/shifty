5c384085c60e0daf45a21ece11d2ffd871f62c22
Øyvind Bakkeli <olivaq@gmail.com>
2015-04-14 21:13:53 +0200

    show username for authenticated users

--- shifty/serializers.py
+++ shifty/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'contactinfo')
-        depth = 0
+        depth = 1
 
 class LimitedUserSerializer(serializers.ModelSerializer):
     class Meta:
