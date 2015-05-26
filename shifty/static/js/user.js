53c77371207f21c4ae3b0ee4f35b3e744673bb93
Jan Drábek <me@jandrabek.cz>
2015-05-26 19:29:48 +0200

    Add management links into events overview Fix invalid currentUser on some occasions

--- shifty/static/js/user.js
+++ shifty/static/js/user.js
@@ -16,23 +16,24 @@ angular.module('cyb.shifty').factory('ShiftsReportService', function ($resource)
 
 
 angular.module('cyb.shifty').factory('AuthService', function($http, localStorageService) {
-  var currentUser = localStorageService.get('user');// {'username':'admin', 'id':1, 'is_staff':true};
-  if(currentUser == null)
-    currentUser = {};
-    if(currentUser == {})
-    {
-        $http.get('/whoami').success(function(user){
-            localStorageService.set('user', user);
-            window.location.reload();
-        });
-    }
-  return {
+    var currentUser = localStorageService.get('user');
+    var backendUser;
 
-    isLoggedIn: function() { currentUser != undefined },
-    currentUser: function() { return currentUser; },
-    isStaff: function() { return currentUser['admin']; },
-    logout: function(){
-        localStorageService.set('user', null);
-    }
-  };
+    $http.get('/whoami').success(function(user){
+        backendUser = user;
+        if (currentUser == null ||  currentUser['id'] != backendUser['id']) {
+            localStorageService.set('user', backendUser);
+            window.location.reload();
+        }
+    });
+
+    return {
+
+        isLoggedIn: function() { return currentUser != undefined },
+        currentUser: function() { return currentUser; },
+        isStaff: function() { return currentUser['admin']; },
+        logout: function(){
+            localStorageService.set('user', null);
+        }
+    };
 });
