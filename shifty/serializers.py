d68969d6e1f884d3b1e31fcdf9e1cbd92a23c85c
Dmitry <filosofov.dmitry@gmail.com>
2015-06-30 20:01:22 +0200

    PATCH and PUT in bulk are working, somehow with help of black magic...

--- shifty/serializers.py
+++ shifty/serializers.py
@@ -1,4 +1,4 @@
-#from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Group
 from models import Event, Shift, ShiftType, User, ShiftEndReport
 from rest_framework import serializers
 from rest_framework import filters
@@ -9,11 +9,13 @@ from rest_framework_bulk import (
     ListBulkCreateUpdateDestroyAPIView,
 )
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'contactinfo')
         depth = 1
+
 
 class LimitedUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,9 +23,11 @@ class LimitedUserSerializer(serializers.ModelSerializer):
         fields = ('username', )
         depth = 1
 
+
 class ShiftEndReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShiftEndReport
+
 
 class ShiftEndReportLightSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,7 +39,7 @@ class ShiftTakeSerializer(serializers.ModelSerializer):
     duration = serializers.ReadOnlyField();
     durationType = serializers.ReadOnlyField()
     volunteer = LimitedUserSerializer()
-    end_report = ShiftEndReportLightSerializer() #serializers.PrimaryKeyRelatedField(read_only=True)#ShiftEndReportLightSerializer()
+    end_report = ShiftEndReportLightSerializer()  #serializers.PrimaryKeyRelatedField(read_only=True)#ShiftEndReportLightSerializer()
 
     def update(self, instance, validated_data):
         print self.intitial_data
@@ -59,18 +63,20 @@ class ShiftTakeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Shift
-        fields = ('id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report')
+        fields = (
+        'id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report')
         depth = 1
 
 
 class ShiftWriteSerializer(serializers.ModelSerializer):
-    # def __init__(self, *args, **kwargs):
-    #     many = kwargs.pop('many', True)
-    #     super(ShiftWriteSerializer, self).__init__(many=many, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        many = kwargs.pop('many', True)
+        super(ShiftWriteSerializer, self).__init__(many=many, *args, **kwargs)
 
     class Meta:
         model = Shift
         fields = ('id', 'event', 'shift_type', 'start', 'stop', 'comment', 'duration')
+
 
 class ShiftSerializer(serializers.ModelSerializer):
     duration = serializers.ReadOnlyField();
@@ -79,11 +85,13 @@ class ShiftSerializer(serializers.ModelSerializer):
     end_report = ShiftEndReportLightSerializer(required=False)
 
     can_change = serializers.SerializerMethodField('canChangeField')
+
     def canChangeField(self, obj):
         request = self.context.get('request', None)
         return obj.end_report.count() == 0
 
     volunteer = serializers.SerializerMethodField('volunteerField')
+
     def volunteerField(self, obj):
         request = self.context.get('request', None)
         if request.user.is_staff or (request.user is not None and request.user.id == obj.volunteer.id):
@@ -91,6 +99,7 @@ class ShiftSerializer(serializers.ModelSerializer):
         return LimitedUserSerializer(instance=obj.volunteer).data
 
     yourshift = serializers.SerializerMethodField('yourshiftField')
+
     def yourshiftField(self, obj):
         request = self.context.get('request', None)
         if request is None:
@@ -101,8 +110,11 @@ class ShiftSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Shift
-        fields = ('id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report', 'yourshift', 'can_change')
+        fields = (
+        'id', 'event', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report',
+        'yourshift', 'can_change')
         depth = 1
+
 
 class EventShiftSerializer(ShiftSerializer):
     duration = serializers.ReadOnlyField();
@@ -110,6 +122,7 @@ class EventShiftSerializer(ShiftSerializer):
     volunteer = LimitedUserSerializer()
 
     volunteer = serializers.SerializerMethodField('volunteerField')
+
     def volunteerField(self, obj):
         if obj.volunteer is None:
             return None
@@ -119,12 +132,14 @@ class EventShiftSerializer(ShiftSerializer):
         return LimitedUserSerializer(instance=obj.volunteer).data
 
     can_change = serializers.SerializerMethodField('canChangeField')
+
     def canChangeField(self, obj):
         request = self.context.get('request', None)
         return obj.end_report.count() == 0
 
 
     end_report = serializers.SerializerMethodField('endReportField')
+
     def endReportField(self, obj):
         request = self.context.get('request', None)
         if not request.user.is_staff:
@@ -132,21 +147,24 @@ class EventShiftSerializer(ShiftSerializer):
         if obj.end_report.count() > 0:
             return ShiftEndReportLightSerializer(instance=obj.end_report.first()).data
         return None
+
     #end_report = serializers.PrimaryKeyRelatedField(read_only=True)#ShiftEndReportLightSerializer()
 
     class Meta:
         model = Shift
-        fields = ('id', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report', 'yourshift', 'can_change')
+        fields = ('id', 'shift_type', 'start', 'stop', 'volunteer', 'comment', 'duration', 'durationType', 'end_report',
+                  'yourshift', 'can_change')
         depth = 1
 
+
 class EventSerializer(serializers.ModelSerializer):
-    shifts = EventShiftSerializer(many=True, read_only=True,)
+    shifts = EventShiftSerializer(many=True, read_only=True, )
     #available = serializers.Field(source='availableShifts')
     responsible = UserSerializer(required=False)
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'description', 'start', 'responsible', 'shifts')#, 'available', 'next', 'previous')
+        fields = ('id', 'title', 'description', 'start', 'responsible', 'shifts')  #, 'available', 'next', 'previous')
         depth = 0
 
 
@@ -170,6 +188,6 @@ class BulkShiftSerializer(BulkSerializerMixin, ShiftWriteSerializer):
         list_serializer_class = BulkListSerializer
 
 
-# class BulkShiftView(ListBulkCreateUpdateDestroyAPIView):
-#     queryset = Shift.objects.all()
-#     serializer_class = BulkShiftSerializer
+        # class BulkShiftView(ListBulkCreateUpdateDestroyAPIView):
+        #     queryset = Shift.objects.all()
+        #     serializer_class = BulkShiftSerializer
